2c0d1520ed95b8be629c25374e11f1f960255759
Jeremy Myers <echelonical@gmail.com>
2022-11-18 15:37:16 -0600

    Updates to post_view, post.py is_auth property

--- spacemonkeyapi/models/post.py
+++ spacemonkeyapi/models/post.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Post(models.Model):
-    author = models.CharField(max_length=155)
     category = models.ForeignKey("Category", on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=155)
     publication_date = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
@@ -11,3 +10,11 @@ class Post(models.Model):
     author = models.ForeignKey("RareUser", on_delete=models.CASCADE, related_name="posts")
     
     tags = models.ManyToManyField("Tag", through="PostTag" )
+
+    @property 
+    def is_author(self):
+        return self.__author
+
+    @is_author.setter
+    def is_author(self, value):
+        self.__author = value
--- spacemonkeyapi/views/post_view.py
+++ spacemonkeyapi/views/post_view.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import action
 from django.http import HttpResponseServerError
+from django.db.models import Case, When, Value, IntegerField, BooleanField
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
@@ -14,7 +15,15 @@ class PostView(ViewSet):
         Returns:
             Response -- JSON serialized post type
         """
+        rare_user = RareUser.objects.get(user=request.auth.user)
         post_view = Post.objects.get(pk=pk)
+        
+        post_view.is_author = False
+
+        if post_view.author == rare_user:
+            post_view.is_author = True
+
+
         serialized = PostSerializer(post_view, context={'request': request})
         return Response(serialized.data, status=status.HTTP_200_OK)
 
@@ -25,13 +34,19 @@ class PostView(ViewSet):
         Returns:
             Response -- JSON serialized list of post types
         """
+        rare_user = RareUser.objects.get(user=request.auth.user)
+
+        post_view = Post.objects.annotate(
+               is_author=Case(
+                   When(author=rare_user,
+                        then=Value(True)),
+                   default=Value(False),
+                   output_field=BooleanField())) \
+                .all()
 
         if "category" in request.query_params:
             post_view = Post.objects.filter(category__id=request.query_params['category'])
         
-        else:
-            post_view = Post.objects.all()
-
         serialized = PostSerializer(post_view, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
 
@@ -43,16 +58,18 @@ class PostView(ViewSet):
             Response -- JSON serialized post instance
         """
         #~ these will need to be updated once the Authentication is Running.
-        # author = Author.objects.get(user=request.auth.user)
+        author = RareUser.objects.get(user=request.auth.user)
         category = Category.objects.get(pk=request.data["category"])
+        tag = Tag.objects.get(pk=request.data["tag"])
 
         post = Post.objects.create(
-            author=request.data["author"],
+            author=author,
             category=category,
             title=request.data["title"],
             publication_date=request.data["publication_date"],
             image_url=request.data["image_url"],
             content=request.data["content"],
+            tag=tag,
             approved=request.data["approved"],
         )
         serializer = PostSerializer(post)
@@ -123,4 +140,7 @@ class PostSerializer(serializers.ModelSerializer):
     category = CategorySerializer(many=False)
     class Meta:
         model = Post
-        fields = ('id', 'author','title', 'publication_date', 'image_url', 'content', 'approved', 'tags', 'comments', 'category',)
+        fields = ('id', 'author', 'is_author',
+        'title', 'publication_date', 'image_url', 'content', 
+        'approved', 'tags', 'comments', 'category',)
+        depth = 1
